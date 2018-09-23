093acfec753d41eafa18756eb0c708c176c4b836
Nikita Shutilin <n_shutilin@twistellar.com>
2018-09-23 14:42:00 +0300

    changed main method code (commit 7)

--- (git)/src/by/bsu/lab1/runner/Main.java
+++ (git)/src/by/bsu/lab1/runner/Main.java
@@ -8,12 +8,12 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Student student1 = new Student("Polo");
+        Student student1 = new Student("Pap");
         student1.addMarks(new int[] {4,4});
         Student student2 = new Student("Ralph");
         student2.addMarks(new int[] {2});
         Student student3 = new Student("Furtado");
-        student2.addMarks(new int[] {2,2,2,2});
+        student2.addMarks(new int[] {2,10});
 
         Group group1 = new Group(Arrays.asList(student1, student2));
         System.out.println(group1.getAverageStudnetsMark());
