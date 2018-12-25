7f4c97e3c66560817bac781bedca5a4a464dd071
Nikita Shutilin <n_shutilin@twistellar.com>
2018-12-26 01:21:52 +0300

    fixed test

--- 10 tests/src/main/java/by/bsu/shutilin/FlightsPage.java
+++ 10 tests/src/main/java/by/bsu/shutilin/FlightsPage.java
@@ -28,7 +28,7 @@ public class FlightsPage {
     By currencyPicklist = By.id("search-currency-change");
     By currencySuffix = By.className("result-fare-suffix");
     By airlineCheckbox = By.xpath("/html/body/main/section[4]/div/div[4]/div[1]/div/div[6]/div/div[1]/div");
-    By airlinesInfo = By.xpath("/html/body/main/section[4]/div/div[4]/div[2]/div[3]/div[7]/div[2]/div[2]/div[1]/div[4]/div[1]");
+    By airlinesInfo = By.xpath("/html/body/main/section[4]/div/div[4]/div[2]/div[3]/div[5]/div[2]/div[2]/div[1]/div[4]/div[1]");
     By cookiesAccept = By.id("gdpr-cookie-accept");
 
     public void getFlightsThroughHomePage() {
@@ -59,6 +59,7 @@ public class FlightsPage {
     }
 
     public String filterByAirlines() {
+        acceptCookies();
         wait.until(ExpectedConditions.elementToBeClickable(airlineCheckbox));
         driver.findElement(airlineCheckbox).click();
         waitLoadEnd();
